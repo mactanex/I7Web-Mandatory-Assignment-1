5cdecaf2faedbe07c1621bbb2688f20ed527202f
Patrick Bjerregaard <pbb@iterator-it.dk>
2019-09-20 12:03:42 +0200

    Added accountSchema

---
+++ app_server/Models/accountDb.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+
+var accountSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        lowercase: true,
+        required: [true, "can't be blank"],
+        match: [/^[a-zA-Z0-9]+$/, 'is invalid'],
+        index: true
+    },
+    email: {
+        type: String,
+        lowercase: true,
+        required: [true, "can't be blank"],
+        match: [/\S+@\S+\.\S+/, 'is invalid'],
+        index: true
+    },
+    hash: String,
+    salt: String
+}, {
+    timestamps: true
+});
+
+mongoose.model('User', accountSchema);
