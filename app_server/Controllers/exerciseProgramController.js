4552704b60eeb770d4d746000a3570f332d11e5f
Malthe Dalgaard Jensen <malthedj@hotmail.com>
2019-09-22 16:38:58 +0200

    exercise Home and add Exercise works

--- app_server/Controllers/exerciseProgramController.js
+++ app_server/Controllers/exerciseProgramController.js
@@ -3,6 +3,8 @@
 var exerciseProgramSchema = require("./../Models/exerciseProgramSchema");
 var accountSchema = require("./../Models/accountSchema");
 
+var currentExerciseProgramTitle = "";
+
 module.exports.ExerciseProgramHomePage = (req, res, next) => {
   res.render("ExerciseProgramHome", { user: req.user });
 };
@@ -13,17 +15,49 @@ module.exports.ExerciseProgramSpecificPage = (req, res, next) => {
 
 module.exports.NewExercise = (req, res, next) => {
   //res.render('index', { title: 'Mandatory Assignment 1 - Exercise Manager' });
-  console.log(req.user);
-  let exercise = {};
+  let exercise = {
+    name: req.body.name,
+    description: req.body.description,
+    set: req.body.set,
+    repsOrTime: req.body.repsOrTime
+  };
+  //const exProgram = req.user.exercisePrograms.find(
+  //  x => x.name == currentExerciseProgramTitle
+  //);
+
+  //console.log(exProgram);
+
+  //find index
+  var index = req.user.exercisePrograms.findIndex(
+    x => x.name == currentExerciseProgramTitle
+  );
+  const exProgram = req.user.exercisePrograms[index];
+  if (index > -1) {
+    req.user.exercisePrograms.splice(index, 1);
+  }
+
+  exProgram.exerciseProgram.push(exercise);
+  //change new to add newly create exercise
+  req.user.exercisePrograms.push(exProgram);
+  // save in db.
+  req.user.save();
+  res.render("ExerciseProgramNew", {
+    title: currentExerciseProgramTitle,
+    list: exProgram.exerciseProgram
+  });
   res.end();
 };
 
 module.exports.NewExerciseProgram = (req, res, next) => {
   //res.render('index', { title: 'Mandatory Assignment 1 - Exercise Manager' });
   let exerciseProgram = { name: req.body.name, exerciseProgram: [] };
+  currentExerciseProgramTitle = exerciseProgram.name;
   req.user.exercisePrograms.push(exerciseProgram);
   req.user.save();
-  res.render("ExerciseProgramNew", { name: req.body.name });
+  res.render("ExerciseProgramNew", {
+    title: req.body.name,
+    list: exerciseProgram.exerciseProgram
+  });
   //res.redirect("/exerciseProgram/newExerciseProgram");
   res.end();
 };
