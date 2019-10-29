b1d640f40c6ccc552eca59b37d4deb61acad328d
Patrick Bjerregaard <pbb@iterator-it.dk>
2019-10-29 18:49:54 +0100

    Pushed some stuff

--- Assignment 2/API/app.js
+++ Assignment 2/API/app.js
@@ -34,7 +34,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 
 app.use("/", apiIndexRouter);
-app.use("/exerciseprogram", apiExerciseRouter);
+app.use("/", apiExerciseRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
