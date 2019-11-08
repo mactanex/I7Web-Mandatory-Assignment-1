09f4a03e951cb038358006b9edbae8367fbc51ec
Patrick Bjerregaard <pbb@iterator-it.dk>
2019-11-08 19:24:06 +0100

    Fixed double firing http requests

--- Assignment 2/Angular/src/app/_Helpers/jwt.interceptor.ts
+++ Assignment 2/Angular/src/app/_Helpers/jwt.interceptor.ts
@@ -1,40 +1,50 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse
+} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-    constructor(private router: Router) {
-    }
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        // add authorization header with jwt token if available
-        // tslint:disable-next-line: max-line-length
-        // why we use localstorage: https://medium.com/@ryanchenkie_40935/angular-authentication-using-the-http-client-and-http-interceptors-2f9d1540eb8
-        // const currentUser = localStorage.GetItem('currentUserToken');
-        const currentUser = JSON.parse(localStorage.getItem('currentUserToken'));
-        if (currentUser) {
-            // currentUser = localStorage.getItem('currentUserToken');
-            request = request.clone({
-                setHeaders: {
-                    Authorization: `Bearer ${currentUser.token}`
-                }
-            });
-            console.log(request);
+  constructor(private router: Router) {}
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    // add authorization header with jwt token if available
+    // tslint:disable-next-line: max-line-length
+    // why we use localstorage: https://medium.com/@ryanchenkie_40935/angular-authentication-using-the-http-client-and-http-interceptors-2f9d1540eb8
+    // const currentUser = localStorage.GetItem('currentUserToken');
+    const currentUser = JSON.parse(localStorage.getItem('currentUserToken'));
+    if (currentUser) {
+      // currentUser = localStorage.getItem('currentUserToken');
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${currentUser.token}`
         }
-        const sub = next.handle(request).subscribe(res => {
-            if (res) {
-            }
-        }, error => {
-            if (error instanceof HttpErrorResponse) {
-                if (error.status === 401) {
-                    this.router.navigate(['']);
-                }
-            }
-        }, () => {
-            sub.unsubscribe();
-        });
-
-        return next.handle(request);
+      });
+      console.log(request);
     }
+    // const sub =
+    next.handle(request); // Subscribing here leads to double firing http requests!
+    // .subscribe(res => {
+    //     if (res) {
+    //     }
+    // }, error => {
+    //     if (error instanceof HttpErrorResponse) {
+    //         if (error.status === 401) {
+    //             this.router.navigate(['']);
+    //         }
+    //     }
+    // }, () => {
+    //     sub.unsubscribe();
+    // });
+
+    return next.handle(request);
+  }
 }
