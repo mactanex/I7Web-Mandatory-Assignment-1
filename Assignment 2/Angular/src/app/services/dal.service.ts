cbbedf59b2926d5ba3cca08797a7277187af5159
Patrick Bjerregaard <pbb@iterator-it.dk>
2019-11-08 13:14:09 +0100

    formatted shit

--- Assignment 2/Angular/src/app/services/dal.service.ts
+++ Assignment 2/Angular/src/app/services/dal.service.ts
@@ -6,33 +6,45 @@ import { Account } from '../Models/Account';
   providedIn: 'root'
 })
 export class DALService {
-
   // exerciseprogramurls
-  private getallexerciseprogramurl = 'http://localhost:3333/exerciseprogram/GetAll'; // to test locally , remember to remove get
-  private getexerciseprogramurl = 'http://localhost:3333/exerciseprogram/:id/Get';
+  private getallexerciseprogramurl =
+    'http://localhost:3333/exerciseprogram/GetAll'; // to test locally , remember to remove get
+  private getexerciseprogramurl =
+    'http://localhost:3333/exerciseprogram/:id/Get';
   private postexerciseprogramurl = 'http://localhost:3333/exerciseprogram/Post';
-  private putexerciseprogramurl = 'http://localhost:3333/exerciseprogram/:id/Put';
-  private deleteexerciseprogramurl = 'http://localhost:3333/exerciseprogram/:id/Delete';
+  private putexerciseprogramurl =
+    'http://localhost:3333/exerciseprogram/:id/Put';
+  private deleteexerciseprogramurl =
+    'http://localhost:3333/exerciseprogram/:id/Delete';
   // userurls
   private loginurl = 'http://localhost:3333/login';
   private signupurl = 'http://localhost:3333/signup';
   // api/get
   private logouturl = 'http://localhost:3333/logout';
   // exerciseurls
-  private getallexerciseurl = 'http://localhost:3333/exerciseprogram/:id/exercise/GetAll';
-  private getexerciseurl = 'http://localhost:3333/exerciseprogram/:id/exercise/:id/Get';
-  private postexerciseurl = 'http://localhost:3333/exerciseprogram/:id/exercise/Post';
-  private putexerciseurl = 'http://localhost:3333/exerciseprogram/:id/exercise/:id/Put';
-  private deleteexerciseurl = 'http://localhost:3333/exerciseprogram/:id/exercise/:id/Delete';
+  private getallexerciseurl =
+    'http://localhost:3333/exerciseprogram/:id/exercise/GetAll';
+  private getexerciseurl =
+    'http://localhost:3333/exerciseprogram/:id/exercise/:id/Get';
+  private postexerciseurl =
+    'http://localhost:3333/exerciseprogram/:id/exercise/Post';
+  private putexerciseurl =
+    'http://localhost:3333/exerciseprogram/:id/exercise/:id/Put';
+  private deleteexerciseurl =
+    'http://localhost:3333/exerciseprogram/:id/exercise/:id/Delete';
   // logurls
-  private getallactivities = 'http://localhost:3333/exerciseprogram/:id/exercise/:id/activity/GetAll';
-  private getactivities = 'http://localhost:3333/exerciseprogram/:id/exercise/:id/activity/:id/Get';
-  private postactivities = 'http://localhost:3333/exerciseprogram/:id/exercise/:id/activity/Post';
-  private putactivities = 'http://localhost:3333/exerciseprogram/:id/exercise/:id/activity/:id/Put';
-  private deleteactivities = 'http://localhost:3333/exerciseprogram/:id/exercise/:id/activity/:id/delete';
+  private getallactivities =
+    'http://localhost:3333/exerciseprogram/:id/exercise/:id/activity/GetAll';
+  private getactivities =
+    'http://localhost:3333/exerciseprogram/:id/exercise/:id/activity/:id/Get';
+  private postactivities =
+    'http://localhost:3333/exerciseprogram/:id/exercise/:id/activity/Post';
+  private putactivities =
+    'http://localhost:3333/exerciseprogram/:id/exercise/:id/activity/:id/Put';
+  private deleteactivities =
+    'http://localhost:3333/exerciseprogram/:id/exercise/:id/activity/:id/delete';
 
-
-  constructor(private client: HttpClient) { }
+  constructor(private client: HttpClient) {}
 
   public signup(user: Account) {
     return this.client.post<Account>(this.signupurl, user); // jwt header ?
