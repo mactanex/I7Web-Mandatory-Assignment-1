e0473a0799e402939071aee84aabd144801a0170
Kristoffer Villadsen <Villadsen67@hotmail.com>
2019-11-08 14:19:19 +0100

    clean up

--- Assignment 2/Angular/src/app/services/dal.service.ts
+++ Assignment 2/Angular/src/app/services/dal.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Account } from '../Models/Account';
 import { exercise } from '../Models/exercise';
-import { environment } from 'ExerciseWorker/src/environments/environment';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -43,32 +43,4 @@ export class DALService {
 
   constructor(private client: HttpClient) {}
 
-  public post<T>(object: T) {
-    return this.client.post<T>(this.getUrl<T>(object), object);
-  }
-
-  public get<T>(object: T) {
-    return this.client.get<T>(this.getUrl<T>(object), object);
-  }
-
-  getUrl<T>(object: T): string {
-    switch (object.constructor) {
-      case exercise:
-        return environment.api;
-      default:
-        break;
-    }
-  }
-
-  public signup(user: Account) {
-    return this.client.post<Account>(this.signupurl, user); // jwt header ?
-  }
-
-  public login(user: Account) {
-    return this.client.post<Account>(this.loginurl, user);
-  }
-
-  public logout() {
-    return this.client.get(this.logouturl);
-  }
 }
