26cb121822b4aec6fd262e9511fd761b91fc3dc0
Kristoffer Villadsen <Villadsen67@hotmail.com>
2019-11-08 11:29:59 +0100

    made form good

--- Assignment 2/Angular/src/app/newexerciseprogram/newexerciseprogram.component.ts
+++ Assignment 2/Angular/src/app/newexerciseprogram/newexerciseprogram.component.ts
@@ -25,37 +25,46 @@ export class NewexerciseprogramComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder ) {
     // this is not viable
-    this.exerciseprogramForm = this.formBuilder.group(
-      {
-        name: '',
-        exercises: this.formBuilder.array([this.createExercise() ])
-      });
    }
 
    createExercise(): FormGroup {
-     return this.formBuilder.group({
-      exercisename: '',
-      exercisedesc: '',
-      exerciseset: 0,
-      exercisereps: ''
-     });
-   }
+    return this.formBuilder.group({
+     exercisename: '',
+     exercisedesc: '',
+     exerciseset: 0,
+     exercisereps: ''
+    });
+  }
 
 
    newExerciseprogram() {
     this.value = true;
-    this.exerciseprogramtemp.exercises.push(new exercise());
+    /* this.exerciseprogramtemp.exercises.push(new exercise()); */
   }
 
   addExercise() {
-    this.exerciseprogramtemp.exercises.push(new exercise());
+    this.exercises = this.exerciseprogramForm.get('exercises') as FormArray;
+    this.exercises.push(this.createExercise());
+   /*  this.exerciseprogramForm.controls.exercises.value.push(this.createExercise()); */
+    /* this.exerciseprogramtemp.exercises.push(new exercise()); */
   }
 
   saveexerciseprogram() {
-  console.log(this.exerciseprogramtemp);
+    this.exerciseprogramtemp.name = this.exerciseprogramForm.value.name;
+    this.exercises = this.exerciseprogramForm.get('exercises') as FormArray;
+    this.exercises.controls.forEach(x => {
+      this.exerciseprogramtemp.exercises.push(x.value);
+    });
+    // remove this console.log and replace with call to backend
+    console.log(this.exerciseprogramtemp);
   }
 
   ngOnInit() {
+    this.exerciseprogramForm = this.formBuilder.group(
+      {
+        name: '',
+        exercises: this.formBuilder.array([ this.createExercise() ])
+      });
   }
 
 }
